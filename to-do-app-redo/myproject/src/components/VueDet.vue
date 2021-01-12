63fd2773796e7ca0eeb0982d0470545f2e93f2cc
bradyvolk <39806319+bradyvolk@users.noreply.github.com>
2021-01-11 22:49:36 -0500

    learning about view binding and inserting html with Vue

--- to-do-app-redo/myproject/src/components/VueDet.vue
+++ to-do-app-redo/myproject/src/components/VueDet.vue
@@ -2,23 +2,41 @@
   <div id="VueDet">
     <h1>Firstname : {{ firstname }}</h1>
     <h1>Lastname : {{ lastname }}</h1>
-    <h1>{{ mydetails() }}</h1>
+    <div v-html="htmlcontent"></div>
+    <img
+      :src="imgsrc"
+      width="300"
+      height="250"
+      v-on:mouseover="changename()"
+      v-on:mouseout="originalname()"
+    />
   </div>
 </template>
 
 <script>
+import tottering_lads from "./../assets/test.jpg";
+
 export default {
   name: "VueDet",
   data() {
     return {
       firstname: "Brady",
       lastname: "Volkmann",
-      address: "New York",
+      htmlcontent: "<div><h1>Vue Js Template</h1></div>",
+      imgsrc: tottering_lads,
     };
   },
   methods: {
     mydetails: function () {
       return "I am " + this.firstname + " " + this.lastname;
+    },
+    changename: function () {
+      this.firstname = "Jon";
+      this.lastname = "Meinhardt";
+    },
+    originalname: function () {
+      this.firstname = "Brady";
+      this.lastname = "Volkmann";
     },
   },
 };
